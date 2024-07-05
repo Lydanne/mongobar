4f82c5e2fbd052a0631f51c9efc3cefe5205cb6b
刘欢 <1829913225@qq.com>
2024-07-05 19:25:12 +0800

    chore: 可以直接定义线程

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+use tokio::runtime::Builder;
+
 mod mongobar;
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    // Mongobar::new("qxg")
-    //     .clean()
-    //     .op_record((
-    //         DateTime::parse_rfc3339_str("2024-07-03T10:54:18.837Z").unwrap(),
-    //         DateTime::parse_rfc3339_str("2024-07-05T10:54:18.838Z").unwrap(),
-    //     ))
-    //     .await?;
-    mongobar::Mongobar::new("qxg").init().op_stress().await?;
+fn main() {
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(1000) // 设置工作线程数
+        .enable_all()
+        .build()
+        .unwrap();
 
-    Ok(())
+    runtime.block_on(async {
+        // Mongobar::new("qxg")
+        //     .clean()
+        //     .op_record((
+        //         DateTime::parse_rfc3339_str("2024-07-03T10:54:18.837Z").unwrap(),
+        //         DateTime::parse_rfc3339_str("2024-07-05T10:54:18.838Z").unwrap(),
+        //     ))
+        //     .await?;
+        if let Err(err) = mongobar::Mongobar::new("qxg").init().op_stress().await {
+            // Handle the error here
+            eprintln!("Error: {}", err);
+        }
+    });
 }
