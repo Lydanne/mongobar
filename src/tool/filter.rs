87d5096ec362a5e883c7d69243a02c020e9148ca
刘欢 <1829913225@qq.com>
2024-08-05 19:02:15 +0800

    refactor: Improve line filtering performance in reg_filter_line function

--- src/tool/filter.rs
+++ src/tool/filter.rs
@@ -3,7 +3,7 @@ use std::io::{stdout, BufRead, Write};
 use crate::{mongobar::op_row::OpRow, utils::to_sha3_8};
 
 pub fn reg_filter_line(target: &str, filter: &str) -> usize {
-    let line_number = 0;
+    let mut line_number = 0;
     let re = regex::Regex::new(filter).unwrap();
     let file = std::fs::File::open(target).unwrap();
     let reader = std::io::BufReader::new(file);
@@ -11,6 +11,7 @@ pub fn reg_filter_line(target: &str, filter: &str) -> usize {
         let line = line.unwrap();
         if re.is_match(&line) {
             println!("{}", line);
+            line_number += 1;
         }
     }
 
@@ -18,7 +19,7 @@ pub fn reg_filter_line(target: &str, filter: &str) -> usize {
 }
 
 pub fn mode_filter_line(target: &str, mode: &str) -> usize {
-    let line_number = 0;
+    let mut line_number = 0;
     let file = std::fs::File::open(target).unwrap();
     let reader = std::io::BufReader::new(file);
     for line in reader.lines() {
@@ -26,6 +27,7 @@ pub fn mode_filter_line(target: &str, mode: &str) -> usize {
         let row = serde_json::from_str::<OpRow>(&line).unwrap();
         if row.build_key() == mode {
             println!("{}", line);
+            line_number += 1;
         }
     }
 
